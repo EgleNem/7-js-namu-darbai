4e2d72e9c4ce517a7dd72659ecd40a8e5502181b
vejuota <egle.nemickaite@gmail.com>
2022-04-12 15:14:35 +0300

    updates

--- ND/04-07-masyvai.js
+++ ND/04-07-masyvai.js
@@ -1,5 +1,4 @@
-const { default: anymatch } = require("anymatch");
-const minimist = require("minimist");
+
 
 console.log('------1------');
 // // 1. Sukurti masyvą, kurio ilgis 20, o reikšmės – skaičiai, nuo 1 iki 20.
@@ -14,28 +13,86 @@ console.log(array);
 console.log('------2------');
 // // 2. Sukurti atsitiktinio ilgio masyvą, nuo 40 iki 60, kurio reikšmės būtų skaičiai nuo 1 iki masyvo ilgio.
 
-
-const array2= [];
-array2.length = Math.floor(Math.random())
-for (let i = 0; i < Math.random(array2.length); i++) {
-    array2.push(Math.floor(Math.random()*(60-40+1)+40));
-
+function rand(min,max) {
+    return Math.floor(Math.random()* (max-min+1)+min);
 }
-console.log(array2);
+
+let masyvas = [];
+
+for (let i = 0; i < rand(40,60); i++) {
+    masyvas.push(rand(1, masyvas.length));
+}
+console.log(masyvas);
 
 console.log('------3------');
 // // 3. Sukurti atsitiktinio ilgio masyva, kurio ilgis nuo 10 iki 15, o reikšmės – atsitiktiniai skaičiai nuo 5
 // iki 47. Surasti didžiausia skaičių masyve, nenaudojant sort ir Math.max funkcijų.
 // console.log('------4------');
 
-arr =[];
-// min.length = 10;
-// max.length = 15;
-arr.length = Math.floor(Math.random()*(15-10+1)+10)
-console.log(arr.length);
+
+let mas1 = [];
+let max = 5;
+
+for (let i = 0; i < rand(10,15); i++) {
+    mas1.push(rand(5, 47));
+}
+console.log(mas1);
+
+for (let i = 0; i < mas1.length; i++){
+    if (mas1[i] > max){
+        max = mas1[i];
+    }
+}
+console.log('max: ' + max);
+
+
 
 // // 4. Sukurti masyvą, kurio ilgis 100, o reikšmės – raidės D, E, F, G, H. 20.
-// console.log('------5------');
+console.log('------5------');
+
+
+
+// const raides = ['D', 'E', 'F', 'G', 'H', 20];
+// const mas2 = [];
+
+// for (let i = 0; i < 100; i++) {
+//     mas2.push(raides(rand()));
+// }
+// console.log(mas2);
+
+
+const raides1 = ['A', 'B', 'C', 'D'];
+
+const masyvas7 = [];
+
+for (let i = 0; i < 100; i++) {
+    masyvas7.push(raides1[rand(0,3)]);
+}
+
+console.log(masyvas7);
+
+let countA = 0;
+let countB = 0;
+let countC = 0;
+let countD = 0;
+
+for (let i = 0; i < masyvas7.length; i++) {
+    if (masyvas7[i] === 'A'){
+        countA++;
+    }
+    if (masyvas7[i] === 'B'){
+        countB++;
+    }
+    if (masyvas7[i] === 'C'){
+        countC++;
+    }
+    if (masyvas7[i] === 'D'){
+        countD++;
+    }
+}
+
+console.log(`Raidžių masyve yra: A: ${countA}, B: ${countB}, C: ${countC}, D: ${countD}`);
+
 // // 5. Sukurti masyvą, kurio ilgis 100, o reikšmės – raidės M,N,O,P. Suskaičiuoti, kurios raidės buvo
 // // daugiausia.
 // console.log('------6------');
